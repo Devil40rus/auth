eecc536daf3a71d3dde7827e872eca40476f3a72
Anatoly <Devil40rus@gmail.com>
2020-04-15 02:11:17 +0300

    v1

---
+++ src/config/store.js
@@ -0,0 +1,21 @@
+import { AsyncStorage } from "react-native";
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import reducers from "../reducers";
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ["authReducer", "userReducer"]
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default () => {
+  let store = createStore(persistedReducer, {}, applyMiddleware(thunk, logger));
+  let persistor = persistStore(store)
+  return { store, persistor }
+}
